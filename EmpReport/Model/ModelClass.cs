7324394a12671754fdf6016e3d5a7a02d537d56a
Nasirul Islam <85866190+Nasirul-Islam@users.noreply.github.com>
2024-12-29 20:21:24 +0600

    try to add report

---
+++ EmpReport/Model/ModelClass.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmpReport.Model
+{
+    public class EmployeeInfo
+    {
+        public int EmpID { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? Email { get; set; }
+        public string? PhoneNumber { get; set; }
+        public DateTime? DateOfBirth { get; set; } = DateTime.Now;
+        public DateTime HireDate { get; set; } = DateTime.Now;
+        public string? JobTitle { get; set; }
+        public string? Department { get; set; }
+        public decimal? Salary { get; set; }
+        public string? Address { get; set; }
+        public string? City { get; set; }
+        public string? State { get; set; }
+        public string? PostalCode { get; set; }
+        public string? Country { get; set; }
+        public bool IsActive { get; set; } = true;
+        public DateTime CreatedDate { get; set; } = DateTime.Now;
+        public DateTime UpdatedDate { get; set; } = DateTime.Now;
+    }
+}
